8c1007f13ce9e8a3a08fec608d4d32282529367e
Gabriel <gabriel.vilen@gmail.com>
2015-02-16 11:40:43 +0100

    init

---
+++ UnisexBathroomProblem/src/unisexbathroomproblem/UnisexBathroomProblem.java
@@ -0,0 +1,16 @@
+package unisexbathroomproblem;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class UnisexBathroomProblem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
